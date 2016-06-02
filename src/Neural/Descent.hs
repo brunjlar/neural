0d478a62d6289df44c22566fd8f41a43644abd68
brunjlar <brunjlar@gmail.com>
2016-06-02 12:29:28 +0200

    descent for mini batches using convolve

--- src/Neural/Descent.hs
+++ src/Neural/Descent.hs
@@ -1,27 +1,71 @@
+{-# OPTIONS_HADDOCK show-extensions #-}
+
 {-# LANGUAGE RankNTypes #-}
+
+{-|
+Module      : Natural.Descent
+Description : gradient descent
+Copyright   : (c) Lars Brünjes, 2016
+License     : MIT
+Maintainer  : brunjlar@gmail.com
+Stability   : experimental
+Portability : portable
+
+This module provides /gradient descent/ for components. 
+A special case of this is the /backpropagation algorithm/ for neural networks.
+-}
 
 module Neural.Descent
     ( Err
+    , descent'
     , descent
     , descentM
     ) where
 
+import Control.Arrow
 import MyPrelude
 import Neural.Component
 import Neural.Monad
 import Utils.Analytic
+import Utils.Arrow
 import Utils.Statistics (mean)
 
+-- | An element @err@ of @'Err' a b c@ transforms a component @c@ into a component @err c@
+--   (with the same weights) that measures @c@'s error.
+--   Then gradient descent can be used to minimize @c@'s error with respect to @err@.
+--
 type Err a b c = forall t. Component' t a b -> Component' t c Analytic
 
-descent :: Component a b -> Err a b c -> Double -> [c] -> (Double, Component a b)
-descent (Component ws c i) err eta xs = (e, Component ws' c i) where
+-- | This function performs one step of gradient descent for the given component and error transformation.
+--
+descent' :: Component a b              -- ^ the component whose error should be decreased 
+            -> Err a b c               -- ^ the error transformation
+            -> Double                  -- ^ the learning rate
+            -> c                       -- ^ a sample
+            -> (Double, Component a b) -- ^ the sample error and the improved component
+descent' (Component ws c i) err eta x = (e, Component ws' c i) where
 
-    (e, ws') = gradient (\w dw -> w - eta * dw) f ws
+    (e, ws') = gradient (\w dw -> w - eta * dw) (runC (err c) x) ws
 
-    f zs = mean [runC (err c) x zs | x <- xs]
+-- | This function performs one step of gradient descent for a 'mini batch' of samples
+--   (by taking the mean over all errors for samples in the batch).
+descent :: Component a b              -- ^ the component whose error should be decreased 
+           -> Err a b c               -- ^ the error transformation
+           -> Double                  -- ^ the learning rate
+           -> [c]                     -- ^ the mini batch of samples
+           -> (Double, Component a b) -- ^ the mean error and the improved component
+descent c err eta xs = descent' c err' eta xs where
 
-descentM :: Monad m => Err a b c -> Double -> [c] -> ComponentM a b m Double
+    err' = \c' -> convolve (err c') >>^ mean
+
+-- | This is the monadic version of 'descent': It performs one step of gradient descent on a 'mini batch'
+--   of samples and implicitly updates the state-component's weights.
+--
+descentM :: Monad m 
+            => Err a b c               -- ^ the error transformation
+            -> Double                  -- ^ the learning rate
+            -> [c]                     -- ^ the mini batch of samples
+            -> ComponentM a b m Double -- ^ the mean error 
 descentM err eta xs = do
     c <- get
     let (e, c') = descent c err eta xs
