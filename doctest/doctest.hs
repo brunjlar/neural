04227df79df4a72cc0f0681420fc1ea06887a0da
brunjlar <brunjlar@gmail.com>
2016-06-04 01:54:23 +0200

    tests and documentation for Utils.Traversable

--- doctest/doctest.hs
+++ doctest/doctest.hs
@@ -2,5 +2,6 @@ import Test.DocTest
 
 main :: IO ()
 main = doctest [ "src/Utils/Matrix.hs"
+               , "src/Utils/Traversable.hs"
                , "src/Utils/Vector.hs"
                ]
--- src/Utils/Traversable.hs
+++ src/Utils/Traversable.hs
@@ -1,3 +1,17 @@
+{-# OPTIONS_HADDOCK show-extensions #-}
+
+{-|
+Module      : Utils.Traversable
+Description : utilities for traversables
+Copyright   : (c) Lars Brünjes, 2016
+License     : MIT
+Maintainer  : brunjlar@gmail.com
+Stability   : experimental
+Portability : portable
+
+This module contains utility functions related to the 'Traversable' typeclass.
+-}
+
 module Utils.Traversable
     ( fromList
     , safeHead
@@ -6,8 +20,25 @@ module Utils.Traversable
 import MyPrelude
 import Utils.Stack
 
+-- | Tries to create a traversable (which must also be applicative) from a list.
+--   If the list contains too few elements, 'Nothing' is returned,
+--
+-- >>> fromList [1, 2, 3] :: Maybe (Identity Int)
+-- Just (Identity 1)
+--
+-- >>> fromList [] :: Maybe (Identity Char)
+-- Nothing
+--
 fromList :: (Applicative t, Traversable t) => [a] -> Maybe (t a)
 fromList xs = sequenceA $ evalStack (sequenceA $ pure pop) xs
 
+-- | Returns the head of a non-empty list or 'Nothing' for the empty list.
+--
+-- >>> safeHead "Haskell"
+-- Just 'H'
+--
+-- >>> safeHead ""
+-- Nothing
+--
 safeHead :: [a] -> Maybe a
 safeHead = (runIdentity <$>) . fromList
