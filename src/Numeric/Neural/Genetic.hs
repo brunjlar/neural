bb6d5ee9a0430d3052af5f27ebe21636c821c9b6
Mark Santolucito <mark.santolucito@yale.edu>
2017-09-23 08:04:17 -0400

    save the prev best

--- src/Numeric/Neural/Genetic.hs
+++ src/Numeric/Neural/Genetic.hs
@@ -23,20 +23,20 @@ import Data.List
 geneticTrain :: 
        ((Model f g a b c) -> IO Double) -- ^ run the model and get cost
        -> Int                            -- ^ generation size
-       -> (Model f g a b c)
+       -> (Double,(Model f g a b c))
        -> Producer (TS f g a b c) IO ()      -- produces better and better models (hopefully)
 geneticTrain f gs ts = loop ts 
    where
-    loop ts' = do
-       newGen <- mapM (lift. evalRandIO. modelR) (replicate gs ts')
+    loop oldBest = do
+       newGen <- mapM (lift. evalRandIO. modelR) (replicate gs (snd oldBest))
        costs <- mapM (lift. f) newGen
        let cs = zip costs newGen
-       let bestModel = maximumBy (\x y -> compare (fst x) (fst y)) cs
+       let bestModel = maximumBy (\x y -> compare (fst x) (fst y)) (oldBest:cs)
        bestModel `deepseq` yield TS
             { tsModel      = snd bestModel
             , tsGeneration = 1 --not sure this is used anyway
             , tsEta        = 0 --also dont think this matters
             , tsBatchError = fst bestModel
             }
-       loop $ snd bestModel
+       loop $ bestModel
     
